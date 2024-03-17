49d5e61121640f880a8d1ceefa98dc77fc89bd98
Muhammad Yusuf <mrmy160602@gmail.com>
2024-03-17 22:15:54 +0700

    add footer

---
+++ src/components/Fragments/Footer.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+    const newLocal = "text-primary border-2"
+    return (
+        <>
+            <hr className={newLocal} />
+            <footer >
+                <div className="container p-3   text-center">
+                        <div className="d-flex align-items-center  justify-content-center">
+                            <img src="/img/logo-health.png" className="me-2 d-inline-block" style={{ height:"30px"}} alt="" />
+                            <h1 className="fw-bold mt-2 text-black">E-HEALTH </h1>
+                        </div>
+                        <p className="col-md-8 m-auto fs-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit sint ex, possimus unde quisquam et tempore optio vero maxime ratione, beatae voluptate corrup.</p>
+                        <div className="d-flex justify-content-center sosmed mt-4">
+                            <Link>
+                                <i className="bi bi-linkedin"></i>
+                            </Link>
+                            <Link>
+                                <i className="bi bi-twitter-x"></i>
+                            </Link>
+                            <Link>
+                                <i className="bi bi-instagram"></i>
+                            </Link>
+                            <Link>
+                                <i className="bi bi-youtube"></i>
+                            </Link>
+                        </div>
+                    <p className="mt-5">Copyright ©2024 e-health | All rights reserved.</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
--- src/components/Layouts/LandingLayout.jsx
+++ src/components/Layouts/LandingLayout.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../Fragments/Navbar"
-
+import Footer from "../Fragments/Footer"
 
 const LandingLayout = ({children}) => {
     return (
         <div>
             <Navbar/>
             {children}
+            <Footer/>
         </div>
     )
 }
