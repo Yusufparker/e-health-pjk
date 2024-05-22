dad12a47819aff05b2b91d54284ba7d900ca3cc5
Muhammad Yusuf <mrmy160602@gmail.com>
2024-05-22 12:33:27 +0700

    update

--- src/components/Fragments/DetailMateri/Page8.jsx
+++ src/components/Fragments/DetailMateri/Page8.jsx
@@ -9,7 +9,16 @@ const Page8 = () => {
             <div className="col-md-8 p-3 d-flex align-items-center">
                 <div className="text-justify">
                     <span className="fs-2 fw-bold me-2">Diabetes melitus</span> dikenal sebagai faktor risiko yang signifikan untuk penyakit jantung koroner. Tingginya kadar glukosa dalam darah pada penderita diabetes dapat merusak pembuluh darah, termasuk arteri koroner yang memasok darah ke jantung. Proses ini dapat menyebabkan pengerasan arteri atau aterosklerosis, yang merupakan karakteristik utama dari penyakit jantung koroner. Selain itu, diabetes juga sering kali menyertai kondisi lain seperti obesitas, hipertensi, dan kadar kolesterol tinggi, yang semuanya juga merupakan faktor risiko yang berkontribusi terhadap perkembangan penyakit jantung koroner (American Heart Association, 2021). 
+                    <p className="mt-3">Selain Diabetes penderita Hipertensi, atau tekanan darah tinggi, juga merupakan faktor risiko utama untuk penyakit jantung koroner. Tingginya tekanan darah dapat menyebabkan kerusakan pada dinding arteri koroner, yang bertanggung jawab atas pasokan darah ke jantung. Seiring waktu, kondisi ini dapat menyebabkan penebalan dan pengerasan arteri koroner, yang dikenal sebagai aterosklerosis, dan mengurangi aliran darah ke jantung. Selain itu, hipertensi juga dapat menyebabkan komplikasi seperti gagal jantung, yang lebih meningkatkan risiko serangan jantung (American Heart Association, 2021).</p>
                 </div>
+            </div>
+        </div>
+        <div className="row fs-14 p-4">
+            <div className="col-md-6 p-3">
+                <p>Kedua hal tersebut memiliki keterkaitan yang erat dalam meningkatkan risiko seseorang terkena penyakit jantung koroner. Dalam kasus diabetes melitus, kerusakan pembuluh darah akibat tingginya kadar glukosa dalam darah mempercepat proses aterosklerosis, sedangkan kondisi hipertensi dapat memperparah kerusakan arteri koroner dan mempercepat kemungkinan terjadinya komplikasi jantung. Dengan demikian, baik diabetes melitus maupun hipertensi memainkan peran penting dalam memperburuk kondisi pembuluh darah dan meningkatkan risiko terkena penyakit jantung koroner (American Heart Association, 2021).</p>
+            </div>
+            <div className="col-md-6 p-3">
+                <img src="/img/tensi.jpeg" className="w-100" alt="" />
             </div>
         </div>
         
